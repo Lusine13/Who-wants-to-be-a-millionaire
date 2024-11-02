f5b4f42ab03bab87f8d175ee0f46972c440c0f01
Lusine13 <lusinevardanyan.a@gmail.com>
2024-11-02 15:50:56 -0700

    adding timer and question-answers in the main component

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,12 @@
+import Questions from '../questions';
+import Timer from '../timer';
 import './index.css';
 
 const Millionaire = () => {
     return (
         <div className="main_container">
-            
+            <Timer />
+            <Questions />            
         </div>
     )
 };
---
+++ src/questions/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+const Questions = () => {
+    return (
+        <div className="questions_container">
+            <div className="question">The republic of Armenia</div>
+            <div className="answers">
+               <div className='answer'>1</div>
+               <div className='answer'>2</div>
+               <div className='answer'>3</div>
+               <div className='answer'>4</div>
+            </div>
+        </div>
+    )
+};
+
+export default Questions;
