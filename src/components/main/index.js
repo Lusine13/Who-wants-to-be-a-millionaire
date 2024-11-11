5d945e8598249893e0034f34ad55bd1affc747b2
Lusine13 <lusinevardanyan.a@gmail.com>
2024-11-11 08:01:54 -0800

    adding 50/50 and call buttons

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -11,21 +11,34 @@ const Millionaire = () => {
     const [fiftyFiftyUsed, setFiftyFiftyUsed] = useState(false); 
     const [callFriendUsed, setCallFriendUsed] = useState(false);
    
+    const restartGame = () => {
+        setQuestionNumber(1);  
+        setStop(false);  
+        setFiftyFiftyUsed(false);  
+        setCallFriendUsed(false);  
+    };
     return (
         <div className="main_container">
             <Timer 
+            stop={stop}
             setStop ={setStop} 
             questionNumber={questionNumber} />
 
             <Questions 
             questionNumber={questionNumber}
             setQuestionNumber={setQuestionNumber}
+            questionAnswers={questionAnswers}
             setStop={setStop}
             fiftyFiftyUsed={fiftyFiftyUsed}
             callFriendUsed={callFriendUsed}
             set50_50Used={setFiftyFiftyUsed}
             setCallUsed={setCallFriendUsed}
-            />            
+            />    
+             {stop && (
+                <button className="restartButton" onClick={restartGame}>
+                    Restart Game
+                </button>
+            )}        
         </div>
     );
 };
